e5f0a29c9e46d7b59dadc496e35da7d4430d064c
Christian Tovar <tovarchristian21>
2019-10-06 11:54:34 -0500

    Upgraded to elixir 1.9

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule CoinPrice.MixProject do
     [
       app: :coin_price,
       version: "0.1.0",
-      elixir: "~> 1.8",
+      elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
